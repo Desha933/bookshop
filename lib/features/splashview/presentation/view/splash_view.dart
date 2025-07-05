55d2d8fdbe048fff047e19c0de98b8e9155d2d37
M.Farar <mostafafarar2030@gmail.com>
2025-07-05 15:36:15 +0300

    first commit

---
+++ lib/features/splashview/presentation/view/splash_view.dart
@@ -0,0 +1,11 @@
+import 'package:bookapp/features/splashview/presentation/view/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: SafeArea(child: SplashViewBody()));
+  }
+}
