a78f7811ea7ae47acf4707e451f5ab26b9d36bb5
M.Farar <mostafafarar2030@gmail.com>
2025-07-07 18:26:08 +0300

    add font text theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:bookapp/contant.dart';
 import 'package:bookapp/features/splashview/presentation/view/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const BookShop());
@@ -14,7 +15,10 @@ class BookShop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
 
       debugShowCheckedModeBanner: false,
       home: const SplashView(),
