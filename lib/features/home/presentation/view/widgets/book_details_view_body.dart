127030ddc83ff4f23761a7866782128c520d4a78
M.Farar <mostafafarar2030@gmail.com>
2025-07-09 14:16:15 +0300

    add book rating to book details

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookapp/contant.dart';
 import 'package:bookapp/core/utils/styles.dart';
+import 'package:bookapp/features/home/presentation/view/widgets/book_rating.dart';
 import 'package:bookapp/features/home/presentation/view/widgets/categry_item.dart';
 import 'package:bookapp/features/home/presentation/view/widgets/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -26,6 +27,8 @@ class BookDetailsViewBody extends StatelessWidget {
             'Rudyard Kipling',
             style: Styles.textstyle16.copyWith(fontStyle: FontStyle.italic),
           ),
+          const SizedBox(height: 18),
+          const BookRating(mainAxisAlignment: MainAxisAlignment.center),
         ],
       ),
     );
