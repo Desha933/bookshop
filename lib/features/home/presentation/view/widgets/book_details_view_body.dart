e380e272e94da0c2c883481eada503a318e27113
M.Farar <mostafafarar2030@gmail.com>
2025-07-09 13:07:19 +0300

    add image  and title and subtitle to details

--- lib/features/home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/book_details_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:bookapp/contant.dart';
+import 'package:bookapp/core/utils/styles.dart';
+import 'package:bookapp/features/home/presentation/view/widgets/categry_item.dart';
 import 'package:bookapp/features/home/presentation/view/widgets/custom_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +9,25 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.sizeOf(context).width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: kPadding),
-      child: Column(children: const [CustomBookDetailsAppBar()]),
+      child: Column(
+        children: [
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
+            child: const CategryItem(),
+          ),
+          const SizedBox(height: 45),
+          const Text('The Jungle Book', style: Styles.textstyle30),
+          const SizedBox(height: 6),
+          Text(
+            'Rudyard Kipling',
+            style: Styles.textstyle16.copyWith(fontStyle: FontStyle.italic),
+          ),
+        ],
+      ),
     );
   }
 }
