d4e97ed70a56aed167e70ddd29bf12642ce3d5fd
M.Farar <mostafafarar2030@gmail.com>
2025-07-14 09:51:07 +0300

    create book model

--- lib/features/search/presentation/view/widgets/search_view_body.dart
+++ lib/features/search/presentation/view/widgets/search_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookapp/contant.dart';
 import 'package:bookapp/core/utils/styles.dart';
-import 'package:bookapp/features/home/presentation/view/widgets/book_list_view_item.dart';
 import 'package:bookapp/features/search/presentation/view/widgets/custom_search_text_field.dart';
+import 'package:bookapp/features/search/presentation/view/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -9,37 +9,28 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kPadding),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          CustomSearchTextField(),
-          SizedBox(height: 16),
-          Text('Result', style: Styles.textstyle18),
-          SizedBox(height: 16),
-          Expanded(child: SearchResultListView()),
-        ],
-      ),
-    );
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      // physics: NeverScrollableScrollPhysics(),
-      // padding: const EdgeInsets.only(left: 20, right: 10),
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: BookListViewItem(),
-        );
-      },
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kPadding),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomSearchTextField(),
+                SizedBox(height: 16),
+                Text('Result', style: Styles.textstyle18),
+                Expanded(child: SizedBox(height: 16)),
+                SizedBox(
+                  height: MediaQuery.sizeOf(context).height * .85,
+                  child: SearchResultListView(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
