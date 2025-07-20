93aa45b8a6cb9fa9897c1ea10f3ba27bb429a776
M.Farar <mostafafarar2030@gmail.com>
2025-07-20 11:51:13 +0300

    setup service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:bookapp/contant.dart';
+import 'package:bookapp/core/utils/api_service.dart';
 import 'package:bookapp/core/utils/app_router.dart';
-import 'package:bookapp/features/splashview/presentation/view/splash_view.dart';
+import 'package:bookapp/core/utils/service_locator.dart';
+import 'package:bookapp/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookapp/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookapp/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BookShop());
 }
 
@@ -15,14 +22,28 @@ class BookShop extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
-      ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create:
+              (context) => FeaturedBooksCubit(HomeRepoImpl(ApiService(Dio()))),
+        ),
+        BlocProvider(
+          create:
+              (context) => NewestBooksCubit(HomeRepoImpl(ApiService(Dio()))),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
 
-      debugShowCheckedModeBanner: false,
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
